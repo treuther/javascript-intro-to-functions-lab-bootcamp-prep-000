1e83b149cfa0c57f9dad40aabab436a278695f6f
Tracy Reuther <tracyreuther@gmail.com>
2019-04-24 00:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma")
+  else if (string === "I love you, Grandma.")
 }
