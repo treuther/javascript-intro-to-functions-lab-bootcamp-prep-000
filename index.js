2d304b77bdec6ffd6101e9b4dc8dc3f4afa46945
Tracy Reuther <tracyreuther@gmail.com>
2019-04-24 00:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
-    return "I love you, too!"
+    return "I love you, too."
   }
 }
